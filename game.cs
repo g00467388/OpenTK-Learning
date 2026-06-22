25864587c94bc68efba79596fc832e4a61a8c1ba
Tomas <g00467388@atu.ie>
2026-06-22 22:16:01 +0100

    Added game.cs (render loop)

--- game.cs
+++ game.cs
@@ -1,22 +1,39 @@
-using System;
+using System.Diagnostics;
+using gl.shaders;
 using OpenTK.Graphics.OpenGL4;
 using OpenTK.Windowing.Common;
 using OpenTK.Windowing.Desktop;
 using OpenTK.Windowing.GraphicsLibraryFramework;
 
+namespace gl;
+
 public class Game : GameWindow
 {
-    private int VertexBufferObject;
-    private int VertexArrayObject;
-    public Shader shader;
+    private readonly Stopwatch _time = new Stopwatch();
+    private int _vertexBufferObject;
+    private int _vertexArrayObject;
+    private Shader? _shader;
+    private Texture? _texture;
+    private int _elementBufferObject;
 
-    float[] vertices =
-    {
-        -0.5f, -0.5f, 0.0f, // Bottom-left
-         0.5f, -0.5f, 0.0f, // Bottom-right
-         0.0f,  0.5f, 0.0f  // Top
-    };
+    private readonly float[] _borderColour = [1.0f, 0.0f, 0.0f, 1.0f];
+    
 
+    private readonly float[] _vertices =
+    [ 
+    //[vertex layout vec 3][texture cords vec 2]
+        0.5f,  0.5f, 0.0f, 1.0f, 1.0f, // top right
+        0.5f, -0.5f, 0.0f, 1.0f, 0.0f, // bottom right
+        -0.5f, -0.5f, 0.0f, 0.0f, 0.0f, // bottom left
+        -0.5f,  0.5f, 0.0f, 0.0f, 1.0f  // top left
+
+    ];
+
+    private readonly uint[] _indices =
+    [
+        0, 1, 2,
+        0, 2, 3
+    ];
     public Game(GameWindowSettings gameWindowSettings, NativeWindowSettings nativeWindowSettings)
         : base(gameWindowSettings, nativeWindowSettings)
     {
@@ -25,41 +42,52 @@ public class Game : GameWindow
     protected override void OnLoad()
     {
         base.OnLoad();
+        _time.Start();
+        _vertexBufferObject = GL.GenBuffer();
+        GL.BindBuffer(BufferTarget.ArrayBuffer, _vertexBufferObject);
+        GL.BufferData(BufferTarget.ArrayBuffer, sizeof(float) * _vertices.Length, _vertices,
+            BufferUsageHint.StaticDraw);
 
-        // Create and compile shader (assumes Shader class handles compilation & linking)
-        shader = new Shader(@"../../../shaders/shader.vert", @"../../../shaders/shader.frag");
 
-        // Generate and bind VBO
-        VertexBufferObject = GL.GenBuffer();
-        GL.BindBuffer(BufferTarget.ArrayBuffer, VertexBufferObject);
-        GL.BufferData(BufferTarget.ArrayBuffer, vertices.Length * sizeof(float), vertices, BufferUsageHint.StaticDraw);
-
-        // Generate and bind VAO
-        VertexArrayObject = GL.GenVertexArray();
-        GL.BindVertexArray(VertexArrayObject);
-
-        // Setup vertex attribute pointers (location = 0)
+        _vertexArrayObject = GL.GenVertexArray();
+        GL.BindVertexArray(_vertexArrayObject);
+        
+        _elementBufferObject = GL.GenBuffer();
+        GL.BindBuffer(BufferTarget.ElementArrayBuffer, _elementBufferObject);
+        GL.BufferData(BufferTarget.ElementArrayBuffer, sizeof(uint) * _indices.Length, _indices, BufferUsageHint.StaticDraw);
+        
+        
+        // attribute aPos
+        GL.VertexAttribPointer(0, 3, VertexAttribPointerType.Float, false, sizeof(float) * 5, 0);
         GL.EnableVertexAttribArray(0);
-        GL.VertexAttribPointer(0, 3, VertexAttribPointerType.Float, false, 3 * sizeof(float), 0);
 
-        // Unbind for cleanliness
-        GL.BindBuffer(BufferTarget.ArrayBuffer, 0);
-        GL.BindVertexArray(0);
+        // attribute aTexCord
+        GL.VertexAttribPointer(1, 2, VertexAttribPointerType.Float, false, sizeof(float) * 5, sizeof(float) * 3);
+        GL.EnableVertexAttribArray(1);
 
-        // Optional: set clear color
-        GL.ClearColor(0.1f, 0.1f, 0.1f, 1.0f);
+     
+        GL.TexParameter(TextureTarget.Texture2D, TextureParameterName.TextureBorderColor, _borderColour);
+        // TODO fix weird file path location 
+        _shader = new Shader("../../../shaders/shader.vert", "../../../shaders/shader.frag");
+        _shader.Use();
+
+        _texture = new Texture("../../../Resources/container.jpg");
+  
+        
     }
+
 
     protected override void OnUnload()
     {
         // Cleanup
         GL.BindBuffer(BufferTarget.ArrayBuffer, 0);
-        GL.DeleteBuffer(VertexBufferObject);
+        GL.DeleteBuffer(_vertexBufferObject);
 
         GL.BindVertexArray(0);
-        GL.DeleteVertexArray(VertexArrayObject);
+        GL.DeleteVertexArray(_vertexArrayObject);
 
-        shader.Dispose();
+
+        _shader?.Dispose();
         base.OnUnload();
     }
 
@@ -73,19 +101,20 @@ public class Game : GameWindow
     protected override void OnRenderFrame(FrameEventArgs e)
     {
         base.OnRenderFrame(e);
-
         GL.Clear(ClearBufferMask.ColorBufferBit | ClearBufferMask.DepthBufferBit);
 
-        shader.Use();
-        GL.BindVertexArray(VertexArrayObject);
-        GL.DrawArrays(PrimitiveType.Triangles, 0, 3);
-
+        GL.BindVertexArray(_vertexArrayObject);
+        GL.DrawElements(PrimitiveType.Triangles, _indices.Length, DrawElementsType.UnsignedInt, 0);
+        
+        _texture?.Use();
+        
         SwapBuffers();
     }
 
     protected override void OnResize(ResizeEventArgs e)
     {
         base.OnResize(e);
+
         GL.Viewport(0, 0, Size.X, Size.Y);
     }
 }
